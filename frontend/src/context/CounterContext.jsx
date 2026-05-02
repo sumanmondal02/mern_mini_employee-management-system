c0024cb314297a2d9d7ea099bb1eea532efeece0
Suman Mondal <sumanmondal@Sumans-MacBook-Pro.local>
2026-05-02 14:59:50 +0530

    Updated frontend for deploy

--- frontend/src/context/CounterContext.jsx
+++ frontend/src/context/CounterContext.jsx
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { useCounterStore } from "../store/counterStore";
+import { useCounterStore } from "../store/CounterStore.js";
 
 // create context
 export const CounterContext = createContext();
